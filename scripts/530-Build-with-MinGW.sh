7aeca368121cf4ed99ab59bb6352227a1bd70c06
Brad Hutchings <brad@componentx.com>
2026-01-20 14:20:04 -0800

    Update 530-Build-with-MinGW.sh

--- scripts/530-Build-with-MinGW.sh
+++ scripts/530-Build-with-MinGW.sh
@@ -97,43 +97,30 @@ if [ "$processor" == "x86_64" ]; then
     export LDFLAGS="-static"
 fi
 
-if [ "$processor" == "aarch64" ]; then
-    # Recent discovery -- cosmo-cc and cosmo-c++ can figure out the -I and -L related to cosmo.
-    # No need to specify them here.
-    export CC="aarch64-unknown-cosmo-cc  \
-        -nostdinc -O3 $ARCH_LEVEL_PARAM"
-    export CXX="aarch64-unknown-cosmo-c++ \
-        -DCOSMOCC=1 -nostdinc -nostdinc++ -O3 -Wno-format-truncation $ARCH_LEVEL_PARAM \
-        -I$(pwd)/openssl/include \
-        -L$(pwd)/openssl/.aarch64/"
-    export AR="cosmoar"
-fi
+# if [ "$processor" == "aarch64" ]; then
+#     # Recent discovery -- cosmo-cc and cosmo-c++ can figure out the -I and -L related to cosmo.
+#     # No need to specify them here.
+#     export CC="aarch64-unknown-cosmo-cc  \
+#         -nostdinc -O3 $ARCH_LEVEL_PARAM"
+#     export CXX="aarch64-unknown-cosmo-c++ \
+#         -DCOSMOCC=1 -nostdinc -nostdinc++ -O3 -Wno-format-truncation $ARCH_LEVEL_PARAM \
+#         -I$(pwd)/openssl/include \
+#         -L$(pwd)/openssl/.aarch64/"
+#     export AR="cosmoar"
+# fi
 
 if [ -v CC ]; then
-    # The OpenSSL linking got moved to vendor/cpp-httplib/CMakeLists.txt.
-    cp vendor/cpp-httplib/CMakeLists.txt vendor/cpp-httplib/CMakeLists-orig.txt
-
-    # Make temporary change to CMake system so we link in static OpenSSL.
-    # sed -i -e 's/PUBLIC OpenSSL::SSL OpenSSL::Crypto/PUBLIC libssl.a libcrypto.a/g' vendor/cpp-httplib/CMakeLists.txt
-    # Delete the rejection test for OpenSSL.
-    # sed -i -e '/#include <openssl\/opensslv.h>/d' vendor/cpp-httplib/CMakeLists.txt
-    # sed -i -e '/error bad version/d' vendor/cpp-httplib/CMakeLists.txt
-
     # Prepare the build folder
     rm -r -f $THIS_BUILD_DIR/$BUILD_SUBDIRECTORY
     cmake -B $BUILD_SUBDIRECTORY -DBUILD_SHARED_LIBS=OFF -DLLAMA_CURL=OFF -DLLAMA_OPENSSL=ON \
       -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_SYSTEM_PROCESSOR=$processor \
       -DCMAKE_VERBOSE_MAKEFILE=$VERBOSE $GGML_PARAMS
 
-    # Revert to original CMake system.
-    # The OpenSSL linking got moved to vendor/cpp-httplib/CMakeLists.txt.
-    # mv vendor/cpp-httplib/CMakeLists-orig.txt vendor/cpp-httplib/CMakeLists.txt
-
     # Build
     cmake --build $BUILD_SUBDIRECTORY --config Release --verbose
 
     # Show off what we built
-    printf "\nBuild of Cosmo $processor of llama.cpp is complete.\n\n"
+    printf "\nBuild of MinGW $processor of llama.cpp is complete.\n\n"
     printf "\$ ls -al $THIS_BUILD_DIR/$BUILD_SUBDIRECTORY/bin/\n"
     ls -al $THIS_BUILD_DIR/$BUILD_SUBDIRECTORY/bin
     printf "\n"

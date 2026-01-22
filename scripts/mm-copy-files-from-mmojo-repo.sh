7c2057e39801faddb9cbfc6746bfdbbd916e6cea
Brad Hutchings <brad@componentx.com>
2026-01-22 10:42:17 -0800

    Update mm-copy-files-from-mmojo-repo.sh

--- scripts/mm-copy-files-from-mmojo-repo.sh
+++ scripts/mm-copy-files-from-mmojo-repo.sh
@@ -17,40 +17,22 @@ cd $HOME
 build_folders=("$BUILD_DIR" "$DOGHOUSE_BUILD_DIR" "$LLAMA_SERVER_BUILD_DIR")
 
 for build_folder in "${build_folders[@]}"; do
-    echo "Build folder: $build_folder"
+    if [ -d "$build_folder" ]; then
+        echo "Copying into: $build_folder/"
+        cp -r $MMOJO_SERVER_FILES/* $build_folder/
+
+        cd $build_folder
+
+        # In tools/server/server-context-mmojo.cpp, replace "defer(" with "defer_task(" to make Cosmo STL happy.
+        sed -i -e 's/defer(/defer_task(/g' tools/server/server-context-mmojo.cpp
+        sed -i -e 's/server_queue::defer(/server_queue::defer_task(/g' tools/server/server-queue.cpp
+        sed -i -e 's/void\ defer(/void\ defer_task(/g' tools/server/server-queue.h
+    fi
 done
-echo "----------"
-
-branding=$1
-
-if [ "$branding" != "doghouse" ] && [ "$branding" != "llama-server" ]; then
-    branding=""
-fi
-
-THIS_BUILD_DIR=$BUILD_DIR
-if [ "$branding" == "doghouse" ]; then
-    THIS_BUILD_DIR=$DOGHOUSE_BUILD_DIR
-elif [ "$branding" == "llama-server" ]; then
-    THIS_BUILD_DIR=$LLAMA_SERVER_BUILD_DIR
-fi
-
-echo "    Branding: $branding"
-echo "copying into: $THIS_BUILD_DIR/"
-echo ""
-
-# This copies the $MMOJO_SERVER_FILES tree into the $BUILD_DIR tree.
-cp -r $MMOJO_SERVER_FILES/* $THIS_BUILD_DIR/
-
-cd $THIS_BUILD_DIR
-
-# In tools/server/server-context-mmojo.cpp, replace "defer(" with "defer_task(" to make Cosmo STL happy.
-sed -i -e 's/defer(/defer_task(/g' tools/server/server-context-mmojo.cpp
-sed -i -e 's/server_queue::defer(/server_queue::defer_task(/g' tools/server/server-queue.cpp
-sed -i -e 's/void\ defer(/void\ defer_task(/g' tools/server/server-queue.h
 
 cd $HOME
 
-printf "\n$STARS\n*\n* FINISHED: $SCRIPT_NAME $1.\n*\n$STARS\n\n"
+printf "\n$STARS\n*\n* FINISHED: $SCRIPT_NAME.\n*\n$STARS\n\n"
 
 ################################################################################
 #  This is an original script for the Mmojo Server repo. It is covered by

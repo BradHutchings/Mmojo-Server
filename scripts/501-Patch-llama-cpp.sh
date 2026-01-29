87e26e6a6d30767053874c43f31232d8411a6e92
Brad Hutchings <brad@componentx.com>
2026-01-28 18:10:59 -0800

    Update 501-Patch-llama-cpp.sh

--- scripts/501-Patch-llama-cpp.sh
+++ scripts/501-Patch-llama-cpp.sh
@@ -87,6 +87,7 @@ fi
 if [ "$branding" == "llama-server" ]; then
     # Update the CMake files.
     sed -i -e 's/common.cpp/common-mmojo.cpp/g' common/CMakeLists.txt
+    sed -i -e 's/server.cpp/server-mmojo.cpp/g' tools/server/CMakeLists.txt
     sed -i -e 's/server-context.cpp/server-context-mmojo.cpp/g' tools/server/CMakeLists.txt
 fi
 

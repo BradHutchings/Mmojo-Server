2293ee8f4058db7223d8b9bdca4dd7fce695c394
Brad Hutchings <brad@componentx.com>
2026-02-06 10:35:44 -0800

    Create mm-start-mmojo-server.sh

---
+++ scripts/mm-start-mmojo-server.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+################################################################################
+# This script adds certs from the Mmojo Share to the mmojo-server.zip packaging
+# file.
+#
+# See licensing note at end.
+################################################################################
+
+SCRIPT_NAME=$(basename -- "$0")
+printf "\n**********\n*\n* STARTED: $SCRIPT_NAME.\n*\n**********\n\n"
+
+if [ -d "$RUN_DIR" ]; then
+    if [ -f "$RUN_DIR/$PACKAGE_MMOJO_SERVER_FILE" ]; then
+        "$RUN_DIR/$PACKAGE_MMOJO_SERVER_FILE"
+        
+    elif [ -f "$RUN_DIR/$PACKAGE_MMOJO_SERVER_APE_FILE" ]; then
+        "$RUN_DIR/$PACKAGE_MMOJO_SERVER_APE_FILE"
+
+    fi
+else
+    echo "The run directory < $RUN_DIR > does not exist. 
+fi
+
+cd $HOME
+
+printf "\n**********\n*\n* FINISHED: $SCRIPT_NAME.\n*\n**********\n\n"
+
+################################################################################
+#  This is an original script for the Mmojo Server repo. It is covered by
+#  the repo's MIT-style LICENSE:
+#
+#  https://github.com/BradHutchings/Mmojo-Server/blob/main/LICENSE
+#
+#  Copyright (c) 2025-26 Brad Hutchings.
+#  --
+#  Brad Hutchings
+#  brad@bradhutchings.com
+################################################################################

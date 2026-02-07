5c72ec82fca814c0d7b71195230647e63030b572
Brad Hutchings <brad@componentx.com>
2026-02-07 15:34:32 -0800

    Update mm-backup-models.sh

--- scripts/mm-backup-models.sh
+++ scripts/mm-backup-models.sh
@@ -15,7 +15,7 @@ cd $HOME
 backed_up_one=0
 
 if [ ! -d "$MMOJO_SHARE_MOUNT_POINT" ]; then
-    echo "You have not created your Mmojo Share mount point."
+    # echo "You have not created your Mmojo Share mount point."
     exit 1
 fi
 

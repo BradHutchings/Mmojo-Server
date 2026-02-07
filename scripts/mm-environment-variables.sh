b1059086dcf8dbaf4cae04334b0c643283d35da4
Brad Hutchings <brad@componentx.com>
2026-02-07 14:35:21 -0800

    Update mm-environment-variables.sh

--- scripts/mm-environment-variables.sh
+++ scripts/mm-environment-variables.sh
@@ -76,10 +76,13 @@ echo "Setting Local and Share Models paths."
 export MOOELS_DIR_NAME="mm-models"
 
 export LOCAL_MODELS_DIR="$HOME/$MOOELS_DIR_NAME"
-export LOCAL_MODEL_MAP="$LOCAL_MODELS_DIR/model-map.txt"
-export LOCAL_DOWNLOAD_MODEL_MAP="$LOCAL_MODELS_DIR/model-queue.txt"
+export LOCAL_MODEL_QUEUE="$LOCAL_MODELS_DIR/model-queue.txt"
 
 export MMOJO_SHARE_MODELS_DIR="$MMOJO_SHARE_MOUNT_POINT/$MOOELS_DIR_NAME"
+
+# Get rid of these.
+export LOCAL_DOWNLOAD_MODEL_MAP="$LOCAL_MODEL_QUEUE"
+export LOCAL_MODEL_MAP="$LOCAL_MODELS_DIR/model-map.txt"
 export MMOJO_SHARE_MODEL_MAP="$MMOJO_SHARE_MODELS_DIR/model-map.txt"
 export MMOJO_SHARE_RESTORE_MODEL_MAP="$MMOJO_SHARE_MODELS_DIR/restore-model-map.txt"
 

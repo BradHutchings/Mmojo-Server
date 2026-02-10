8a77ccd5a614246a7915c3414f9431aaaf5a3699
Brad Hutchings <brad@componentx.com>
2026-02-09 18:49:32 -0800

    Create 07-Autostart-Mmojo-Server.sh

---
+++ deploy/200-Windows-WSL/07-Autostart-Mmojo-Server.sh
@@ -0,0 +1,29 @@
+## 07. Autostart Mmojo Server
+### About this Step (Optional)
+In this short step, we're going to modify the Mmojo Server WSL instance, it automatically starts Mmojo Server. This will enable a workflow where you click the Mmojo Server icon in the Taskbar to launch it, and close to window to stop it.
+
+---
+### Autostart Mmojo Server
+Run this command:
+```
+if ! grep -q "mm-start-mmojo-server.sh" "$HOME/.bashrc"; then
+cat << EOF >> $HOME/.bashrc
+mm-start-mmojo-server.sh
+EOF
+fi
+```
+
+Now, close your Mmojo Server WSL window, wait a few moments, then click the Mmojo Server icon in your Taskbar to see autostart in action.
+
+I told you it was a short step!
+
+---
+### Proceed
+- **Next:** This is the last step in this section.
+- **Previous:** [06. Control Mmojo Server](06-Control-Mmojo-Server.md)
+- **Up:** [Deploy Mmojo Server on Windows (WSL)](README.md)
+
+---
+[MIT-Style License](/LICENSE)<br/>
+Copyright (c) 2025-26 [Brad Hutchings](mailto:brad@bradhutchings.com)<br/>
+[https://github.com/BradHutchings/Mmojo-Server](https://github.com/BradHutchings/Mmojo-Server)

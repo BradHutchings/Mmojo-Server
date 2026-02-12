16df20cf8251e17a7f08e2d72530dbb0be3a6da9
Brad Hutchings <brad@componentx.com>
2026-02-11 18:35:10 -0800

    Update mm-environment-variables.sh

--- scripts/mm-environment-variables.sh
+++ scripts/mm-environment-variables.sh
@@ -113,7 +113,7 @@ export BUILD_EXECUTABLE_PERFORMANT_AARCH64="build-executable-performant-aarch64"
 export BUILD_EXECUTABLE_NATIVE_X86_64="build-executable-native-x86_64"
 export BUILD_EXECUTABLE_NATIVE_AARCH64="build-executable-native-aarch64"
 
-export BUILD_EXECUTABLE_PI5_AARCH64="build-executable-pi5-aarch64"
+export BUILD_EXECUTABLE_RPI5_AARCH64="build-executable-rpi5-aarch64"
 
 export BUILD_MINGW_COMPATIBLE_X86_64="build-mingw-compatible-x86_64"
 export BUILD_MINGW_COMPATIBLE_AARCH64="build-mingw-compatible-aarch64"

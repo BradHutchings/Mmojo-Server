4e21cdc1f1ff8b8102621392e5dbf458a899b10c
Brad Hutchings <brad@componentx.com>
2026-02-12 11:38:37 -0800

    Update mm-start-mmojo-server.sh

--- scripts/mm-start-mmojo-server.sh
+++ scripts/mm-start-mmojo-server.sh
@@ -34,7 +34,7 @@ else
 fi
 
 mmojoServerRunning=$(pgrep "mmojo-server")
-if ! $mmojoServerRunning ; then
+if [ "$mmojoServerRunning" != "" ] ; then
     if [ -f "$MMOJO_SERVER_EXEC" ]; then
         if ($runInBackground); then
             echo "Starting mmojo-server in the background."

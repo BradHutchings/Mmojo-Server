df52b83acdf7c1bc6921b1b78eb6057145c44960
Brad Hutchings <brad@componentx.com>
2026-02-19 09:20:53 -0800

    Update mm-prepare-clone-llama-cpp.sh

--- scripts/mm-prepare-clone-llama-cpp.sh
+++ scripts/mm-prepare-clone-llama-cpp.sh
@@ -23,14 +23,12 @@ cd $HOME
 
 branding=$1
 
-if [ "$branding" != "doghouse" ] && [ "$branding" != "llama-server" ]; then
+if [ "$branding" != "llama-server" ]; then
     branding=""
 fi
 
 THIS_BUILD_DIR=$BUILD_DIR
-if [ "$branding" == "doghouse" ]; then
-    THIS_BUILD_DIR=$DOGHOUSE_BUILD_DIR
-elif [ "$branding" == "llama-server" ]; then
+if [ "$branding" == "llama-server" ]; then
     THIS_BUILD_DIR=$LLAMA_SERVER_BUILD_DIR
 fi
 

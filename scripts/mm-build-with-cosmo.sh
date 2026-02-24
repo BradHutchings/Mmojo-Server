01b1e387ee7e8d9be355c138950e8ab4bea8a7ef
Brad Hutchings <brad@componentx.com>
2026-02-24 04:53:11 -0800

    Update mm-build-with-cosmo.sh

--- scripts/mm-build-with-cosmo.sh
+++ scripts/mm-build-with-cosmo.sh
@@ -122,10 +122,10 @@ if [ -v CC ]; then
     mv vendor/cpp-httplib/CMakeLists-orig.txt vendor/cpp-httplib/CMakeLists.txt
 
     # Build
-    cmake --build $BUILD_SUBDIRECTORY --config Release
+    cmake --build "$BUILD_SUBDIRECTORY" --config Release
 
     echo
-    echo "Build command: cmake --build $BUILD_SUBDIRECTORY --config Release"
+    echo "Build command: cmake --build \"$BUILD_SUBDIRECTORY\" --config Release"
 
     # Show off what we built
     printf "\nBuild of Cosmo $processor of llama.cpp is complete.\n\n"

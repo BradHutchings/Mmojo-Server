b5a518ac67f458f62c63e16a2f3d8a87dfa96c45
Brad Hutchings <brad@componentx.com>
2026-02-24 09:34:50 -0800

    Update mm-repo-branch-main.sh

--- scripts/mm-repo-branch-main.sh
+++ scripts/mm-repo-branch-main.sh
@@ -18,20 +18,20 @@ if [ -d "$MMOJO_SERVER_REPO_DIR" ]; then
   cd $WD
 
   # These are the scripts. They need to be executable.
-  chmod -f a+x $MMOJO_SERVER_SCRIPTS/2*.sh
-  chmod -f a+x $MMOJO_SERVER_SCRIPTS/3*.sh
-  chmod -f a+x $MMOJO_SERVER_SCRIPTS/4*.sh
-  chmod -f a+x $MMOJO_SERVER_SCRIPTS/5*.sh
-  chmod -f a+x $MMOJO_SERVER_SCRIPTS/6*.sh
+  chmod -f a+x $MMOJO_SERVER_REPO_SCRIPTS/2*.sh
+  chmod -f a+x $MMOJO_SERVER_REPO_SCRIPTS/3*.sh
+  chmod -f a+x $MMOJO_SERVER_REPO_SCRIPTS/4*.sh
+  chmod -f a+x $MMOJO_SERVER_REPO_SCRIPTS/5*.sh
+  chmod -f a+x $MMOJO_SERVER_REPO_SCRIPTS/6*.sh
   chmod -f a+x $LLAMA_SERVER_SCRIPTS/4*.sh
   chmod -f a+x $LLAMA_SERVER_SCRIPTS/5*.sh
   chmod -f a+x $LLAMA_SERVER_SCRIPTS/6*.sh
 
   ### Links don't work - end up modifying repo files on chmod.
-  cp $MMOJO_SERVER_SCRIPTS/mm-*.sh $HOME_SCRIPTS
+  cp $MMOJO_SERVER_REPO_SCRIPTS/mm-*.sh $HOME_SCRIPTS
   chmod a+x $HOME_SCRIPTS/mm-*.sh
 
-  cp $MMOJO_SERVER_SCRIPTS/mr-*.sh $HOME_SCRIPTS
+  cp $MMOJO_SERVER_REPO_SCRIPTS/mr-*.sh $HOME_SCRIPTS
   chmod a+x $HOME_SCRIPTS/mr-*.sh
 
   if [ -d "$BUILD_DIR" ]; then

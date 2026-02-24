6066c0b03b3d0296f6d19040f1338dcb56e71337
Brad Hutchings <brad@componentx.com>
2026-02-24 09:21:47 -0800

    Update mm-environment-variables.sh

--- scripts/mm-environment-variables.sh
+++ scripts/mm-environment-variables.sh
@@ -33,9 +33,9 @@ export MOOELS_DIR_NAME="mm-models"
 export PACKAGES_DIR_NAME="mm-packages"
 
 echo "Setting mmojo-server paths."
-export MMOJO_SERVER_DIR="$HOME/mm-mmojo-server-repo"
-export MMOJO_SERVER_FILES="$MMOJO_SERVER_DIR/files"
-export MMOJO_SERVER_SCRIPTS="$MMOJO_SERVER_DIR/scripts"
+export MMOJO_SERVER_REPO_DIR="$HOME/mm-mmojo-server-repo"
+export MMOJO_SERVER_FILES="$MMOJO_SERVER_REPO_DIR/files"
+export MMOJO_SERVER_SCRIPTS="$MMOJO_SERVER_REPO_DIR/scripts"
 
 echo "Setting Mmojo Share paths."
 if [ "$(uname -s)" = "Darwin" ]; then

3cf6b9485de614500dd65cd55d013e16f8c95c5d
Brad Hutchings <brad@componentx.com>
2026-02-24 04:31:51 -0800

    Update mm-build-with-cosmo.sh

--- scripts/mm-build-with-cosmo.sh
+++ scripts/mm-build-with-cosmo.sh
@@ -110,8 +110,10 @@ if [ -v CC ]; then
     sed -i -e '/error bad version/d' vendor/cpp-httplib/CMakeLists.txt
 
     # Prepare the build folder
-    rm -r -f $THIS_BUILD_DIR/$BUILD_SUBDIRECTORY
-    cmake -B $BUILD_SUBDIRECTORY -DBUILD_SHARED_LIBS=OFF -DLLAMA_CURL=OFF -DLLAMA_OPENSSL=ON \
+    if [ "$THIS_BUILD_DIR/$BUILD_SUBDIRECTORY" != "" ]; then
+        rm -r -f "$THIS_BUILD_DIR/$BUILD_SUBDIRECTORY"
+    fi
+    cmake -B "$BUILD_SUBDIRECTORY" -DBUILD_SHARED_LIBS=OFF -DLLAMA_CURL=OFF -DLLAMA_OPENSSL=ON \
       -DCMAKE_SYSTEM_NAME=Linux -DCMAKE_SYSTEM_PROCESSOR=$processor \
       -DCMAKE_VERBOSE_MAKEFILE=$VERBOSE $GGML_PARAMS
 

403dbf773f4991138330c7452b1046107c53625f
Brad Hutchings <brad@componentx.com>
2026-02-25 19:42:29 -0800

    Update proxy-watcher.py

--- files/proxy/proxy-watcher.py
+++ files/proxy/proxy-watcher.py
@@ -4,13 +4,18 @@
 
 import tkinter as tk
 from pathlib import Path
+import threading
+import datetime
+import json
+import time
 
 #----------------------------------------
 # values are the request file names.
 # value is the active request file name.
 #----------------------------------------
 
-NO_REQUEST = "No request."
+NO_REQUESTS = "No requests."
+REQUESTS_PATH = "requests/"
 
 values=[]
 value = ""
@@ -28,7 +33,7 @@ def loadValues():
     global values, value
     values.clear()
     try:
-        p = Path("requests")
+        p = Path(REQUESTS_PATH)
         for entry in p.iterdir():
             values.append(entry.name)
     except:
@@ -44,7 +49,7 @@ def setValue(value_index):
     elif (len(values) > 0):
         value = values[0]
     else:
-        value = NO_REQUEST
+        value = NO_REQUESTS
 
 def prev():
     global values, value
@@ -67,31 +72,109 @@ def next():
     setValue(value_index)
     showValue()
 
-def showValue():
-    global values, value
-    request.config(text=value)
-
-loadValues()
-
 #----------------------------------------
 # Control layout.
 #----------------------------------------
 
-root = tk.Tk()
-root.geometry("800x600")
-root.title("Mmojo Proxy Watcher")
+ui_root = tk.Tk()
+ui_root.geometry("800x600")
+ui_root.title("Mmojo Proxy Watcher")
 
-top_frame = tk.Frame(root)
-top_frame.pack(padx=(20, 20), pady=(12, 12))
+ui_top_frame = tk.Frame(ui_root)
+ui_top_frame.pack(side = "top", fill = "both", padx = (20, 20), pady = (12, 20))
 
-prev_button = tk.Button(top_frame, text="<", command=prev)
-request = tk.Label(top_frame, text="", font=("Helvetica", 24))
-next_button = tk.Button(top_frame, text=">", command=next)
+ui_data_frame = tk.Frame(ui_root)
+ui_data_frame.pack(side = "top", fill = "both", expand = True, padx = (20, 20), pady = (0, 12))
 
-prev_button.grid(row=0, column=0, padx=(0, 12))
-request.grid(row=0, column=1, padx=(0, 12))
-next_button.grid(row=0, column=2, padx=(0, 0))
+ui_prev_button = tk.Button(ui_top_frame, text = "<", command = prev)
+ui_request = tk.Label(ui_top_frame, text = "", font = ("Helvetica", 18))
+ui_next_button = tk.Button(ui_top_frame, text = ">", command = next)
+ui_path = tk.Label(ui_top_frame, text = "(Path goes here.)", anchor = "w", font = ("Helvetica", 18))
 
-showValue()
+ui_request_data = tk.Text(ui_data_frame, width = 20, height = 10, wrap = "word", background = "#E0E0E0", relief = "solid")
+ui_response_data = tk.Text(ui_data_frame, width = 20, height = 10, wrap = "word", background = "#F0F0F0", relief = "solid")
 
-root.mainloop()
+ui_prev_button.pack(side="left", padx = (0, 12))
+ui_request.pack(side="left", padx = (0, 12))
+ui_next_button.pack(side="left", padx = (0, 12))
+ui_path.pack(side = "left", fill = "both", expand = True, padx = (0,0))
+
+ui_request_data.pack(side = "left", fill = "both", expand = True, padx = (0, 12))
+ui_response_data.pack(side = "left", fill = "both", expand = True, padx = (0, 0))
+
+ui_request_data.config(state = tk.DISABLED )
+ui_response_data.config(state = tk.DISABLED )
+
+#----------------------------------------
+# Timer for repeatedly loading.
+#----------------------------------------
+
+def timerLoadValues():
+    global ui_root
+    loadValues()
+    showValue()
+    ui_root.update()
+
+    timer = threading.Timer(3.0, timerLoadValues)
+    timer.start()
+
+#----------------------------------------
+# showValue loads the data from the file.
+#----------------------------------------
+
+def showValue():
+    global values, value
+    ui_request.config(text=value)
+    request_data = {}
+    tries = 0
+    if (value != ""):
+        while (tries < 5) and not request_data:
+            try:
+                with open(REQUESTS_PATH + value, 'r') as file:
+                    request_data = json.load(file)
+            except Exception as e:
+                print("exception: " + str(e))
+                request_data = {}
+                time.sleep(0.5)
+            tries = tries + 1
+
+    if "path" in request_data:
+        ui_path.config(text = request_data["path"])
+    else:
+        ui_path.config(text = "")
+
+    ui_request_data.config(state = tk.NORMAL )
+    ui_request_data.delete("1.0", tk.END)
+    if "request_body" in request_data:
+        ui_request_data.insert("1.0", str(request_data["request_body"]))
+    else:
+        ui_request_data.insert("1.0", "Nothing to see here.")
+    ui_request_data.config(state = tk.DISABLED )
+
+
+    new_response_data = "Nothing to see here."
+    if "response" in request_data:
+        new_response_data = str(request_data["response"])
+    old_response_data = ui_response_data.get("1.0", tk.END)
+
+    # the data from the Text has a \n tacked on the end. Grrrrr.
+    if (new_response_data != old_response_data) and ((new_response_data + "\n") != old_response_data):
+        print("Replacing old_response_data.\n----------")
+        save_scroll = ui_response_data.yview()[0]
+        ui_response_data.config(state = tk.NORMAL )
+        ui_response_data.delete("1.0", tk.END)
+        ui_response_data.insert("1.0", new_response_data)
+        ui_response_data.config(state = tk.DISABLED )
+
+        # This scrolls a percent, not to a particular line. Will revisit.
+        if new_response_data.startswith(old_response_data):
+            ui_response_data.yview("moveto", save_scroll)
+        if new_response_data.startswith(old_response_data[:-1]):
+            ui_response_data.yview("moveto", save_scroll)
+
+#----------------------------------------
+# Main control.
+#----------------------------------------
+
+timerLoadValues()
+ui_root.mainloop()

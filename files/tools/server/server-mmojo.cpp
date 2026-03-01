8fa6c945aba6d6332eae2c3b26d5b33313fbb342
Brad Hutchings <brad@componentx.com>
2026-02-28 22:54:42 -0800

    Refactor server-mmojo.cpp by removing unused functions
    Removed deprecated diagnostic functions and cleaned up code.

--- files/tools/server/server-mmojo.cpp
+++ files/tools/server/server-mmojo.cpp
@@ -321,214 +321,6 @@ void main_mmojo_server_1(char* argv_0) {
         find_first_gguf(workingDirectoryPath, firstGgufPath);
     }
 }
-
-void main_path_diagnostics() {
-    printf("\n");
-    printf("- main_path_diagnostics()\n");
-
-    printf("\n");
-    printf("  - Paths of things we care about:\n");
-    mmojo_printf("    -       executablePath: %s\n", (const char*) executablePath.c_str());
-    mmojo_printf("    - executableParentPath: %s\n", (const char*) executableParentPath.c_str());
-    mmojo_printf("    - workingDirectoryPath: %s\n", (const char*) workingDirectoryPath.c_str());
-    mmojo_printf("    -             argsPath: %s\n", (const char*) argsPath.c_str());
-    mmojo_printf("    -          supportPath: %s\n", (const char*) supportPath.c_str());
-    mmojo_printf("    -      supportArgsPath: %s\n", (const char*) supportArgsPath.c_str());
-    mmojo_printf("    -              zipPath: %s\n", (const char*) zipPath.c_str());
-    mmojo_printf("    -          zipArgsPath: %s\n", (const char*) zipArgsPath.c_str());
-    mmojo_printf("    -        firstGgufPath: %s\n", (const char*) firstGgufPath.c_str());
-
-    printf("\n");
-    printf("- These paths exist:\n");
-    if (std::filesystem::exists(executablePath)) {
-        mmojo_printf("    -       executablePath exists: %s\n", (const char*) executablePath.c_str());
-    }
-    if (std::filesystem::exists(executableParentPath)) {
-        mmojo_printf("    - executableParentPath exists: %s\n", (const char*) executableParentPath.c_str());
-    }
-    if (std::filesystem::exists(workingDirectoryPath)) {
-        mmojo_printf("    - workingDirectoryPath exists: %s\n", (const char*) workingDirectoryPath.c_str());
-    }
-    if (std::filesystem::exists(argsPath)) {
-        mmojo_printf("    -             argsPath exists: %s\n", (const char*) argsPath.c_str());
-    }
-    if (std::filesystem::exists(supportPath)) {
-        mmojo_printf("    -          supportPath exists: %s\n", (const char*) supportPath.c_str());
-    }
-    if (std::filesystem::exists(supportArgsPath)) {
-        mmojo_printf("    -      supportArgsPath exists: %s\n", (const char*) supportArgsPath.c_str());
-    }
-    if (std::filesystem::exists(zipPath)) {
-        mmojo_printf("    -              zipPath exists: %s\n", (const char*) zipPath.c_str());
-    }
-    if (std::filesystem::exists(zipArgsPath)) {
-        mmojo_printf("    -          zipArgsPath exists: %s\n", (const char*) zipArgsPath.c_str());
-    }  
-    if (std::filesystem::exists(firstGgufPath)) {
-        mmojo_printf("    -        firstGgufPath exists: %s\n", (const char*) firstGgufPath.c_str());
-    }  
-}
-
-void main_args_files(int& argc, char **& argv) {
-    printf("\n");
-    printf("- main_args_files()\n");
-
-    // Implement an args file feature inspired by llamafile's.
-    // It does not require Cosmo anymore, as the mmojo_args function is part of mmojo-server now.
-    // This is where we modify argc and argv!!
-
-    // At this point, argc, argv represent:
-    //     command (User supplied args)
-
-    #if defined(_WIN32)
-    using convert_type = std::codecvt_utf8<wchar_t>;
-    std::wstring_convert<convert_type, wchar_t> converter;
-    #endif
-  
-    if (std::filesystem::exists(argsPath)) {
-        #if defined(_WIN32)
-        const std::string& filename = converter.to_bytes(argsPath.c_str());
-        #else
-        const std::string& filename = (const char*) argsPath.c_str();
-        #endif
-
-        printf("  - using argsPath: %s\n", (const char*) filename.c_str());
-        argc = mmojo_args((const char*) filename.c_str(), &argv);
-    }
-
-    // At this point, argc, argv represent:
-    //     command (argsPath args) (User supplied args)
-
-    if (std::filesystem::exists(supportArgsPath)) {
-        #if defined(_WIN32)
-        const std::string& filename = converter.to_bytes(supportArgsPath.c_str());
-        #else
-        const std::string& filename = (const char*) supportArgsPath.c_str();
-        #endif
-
-        printf("  - using argsPath: %s\n", (const char*) filename.c_str());
-        argc = mmojo_args((const char*) filename.c_str(), &argv);
-    }
-
-    // At this point, argc, argv represent:
-    //     command (supportArgsPath args) (argsPath args) (User supplied args)
-
-    #ifdef COSMOCC
-    if (std::filesystem::exists(zipArgsPath)) {
-        // We don't use MinGW to compile for Windows under COSMOCC
-        argc = mmojo_args((const char*) zipArgsPath.c_str(), &argv);
-    }
-
-    // At this point, argc, argv represent:
-    //     command (zipArgsPath args) (supportArgsPath args) (argsPath args) (User supplied args)
-    #endif
-
-    // Yep, this is counterintuitive, but how the mmojo_args command works.
-}
-
-void main_mmojo_server_2(common_params& params) {
-    printf("\n");
-    printf("- main_mmojo_server_2()\n");
-
-    printf("  - Checking for missing model and model inside APE zip.\n");
-    
-    // If we have no model path at this point, use the firstGgufPath.
-    // I think I have all the possibilities for specifying a model covered here.
-    if ((params.model.path == "") && (params.model.url == "") && (params.model.docker_repo == "") &&  
-        (params.model.hf_repo == "") && (params.model.hf_file == "") && 
-        std::filesystem::exists(firstGgufPath)) {
-
-        #if defined(_WIN32)
-        using convert_type = std::codecvt_utf8<wchar_t>;
-        std::wstring_convert<convert_type, wchar_t> converter;
-        const std::string& firstGgufPathString = converter.to_bytes(firstGgufPath.c_str());
-
-        printf("  - Using firstGgufPath for model: %s\n", (const char*) firstGgufPathString.c_str());
-        params.model.path = (const char*) firstGgufPathString.c_str();
-        #else
-        printf("  - Using firstGgufPath for model: %s\n", (const char*) firstGgufPath.c_str());
-        params.model.path = (const char*) firstGgufPath.c_str();
-        #endif
-    }
-    
-    #ifdef COSMOCC
-    const std::string zipPathSlash = "/zip/";
-    if (starts_with(params.model.path, zipPathSlash)) {
-        // if the gguf is in the zip file, we have to turn off use_map.
-        printf("  - The model file is in /zip, so turning off use_mmap.\n\n");
-        params.use_mmap = false;
-    }
-    #endif
-}
-
-void main_mmojo_server_3(common_params& params) {
-    printf("\n");
-    printf("- main_mmojo_server_3()\n");
-
-  // This could be automated by looking for "common_init();" and inserting this block immediately after. -Brad 2025-11-05
-    // fix params -- model, path, ssl-key-file, ssl-cert-file
-    // if they are relative paths, fix to absolute relative to working directory
-    if (supportPath != "") {
-        const std::string& supportRootPath = "/support/";
-        std::string supportPathSlash = (const char*) supportPath.c_str();
-        supportPathSlash += "/";
-      
-        if (starts_with(params.model.path, supportRootPath)) {
-            printf("\n");
-            mmojo_printf("--model path starts with %s.\n", (const char*) supportRootPath.c_str());
-            params.model.path.replace(0, supportRootPath.length(), (const char*) supportPathSlash.c_str());
-            printf("  - new model path: %s\n", (const char*) params.model.path.c_str());
-        }
-        if (starts_with(params.public_path, supportRootPath)) {
-            printf("\n");
-            mmojo_printf("--path path starts with %s.\n", (const char*) supportRootPath.c_str());
-            params.public_path.replace(0, supportRootPath.length(), (const char*) supportPathSlash.c_str());
-            printf("  - new path path: %s\n", (const char*) params.public_path.c_str());
-        }
-        if (starts_with(params.ssl_file_key, supportRootPath)) {
-            printf("\n");
-            mmojo_printf("--ssl-key-file path starts with %s.\n", (const char*) supportRootPath.c_str());
-            params.ssl_file_key.replace(0, supportRootPath.length(), (const char*) supportPathSlash.c_str());
-            printf("  - new ssl-key-file path: %s\n", (const char*) params.ssl_file_key.c_str());
-        }
-        if (starts_with(params.ssl_file_cert, supportRootPath)) {
-            printf("\n");
-            mmojo_printf("--ssl-cert-file path starts with %s.\n", (const char*) supportRootPath.c_str());
-            params.ssl_file_cert.replace(0, supportRootPath.length(), (const char*) supportPathSlash.c_str());
-            printf("  - new ssl-cert-file path: %s\n", (const char*) params.ssl_file_cert.c_str());
-        }
-    }
-    if (executableParentPath != "") {
-        const std::string& executableRootPath = "/app/";
-        std::string executableParentPathSlash = (const char*) executableParentPath.c_str();
-        executableParentPathSlash += "/";
-      
-        if (starts_with(params.model.path, executableRootPath)) {
-            printf("\n");
-            mmojo_printf("--model path starts with %s.\n", (const char*) executableRootPath.c_str());
-            params.model.path.replace(0, executableRootPath.length(), (const char*) executableParentPathSlash.c_str());
-            printf("  - new model path: %s\n", (const char*) params.model.path.c_str());
-        }
-        if (starts_with(params.public_path, executableRootPath)) {
-            printf("\n");
-            mmojo_printf("--path path starts with %s.\n", (const char*) executableRootPath.c_str());
-            params.public_path.replace(0, executableRootPath.length(), (const char*) executableParentPathSlash.c_str());
-            printf("  - new path path: %s\n", (const char*) params.public_path.c_str());
-        }
-        if (starts_with(params.ssl_file_key, executableRootPath)) {
-            printf("\n");
-            mmojo_printf("--ssl-key-file path starts with %s.\n", (const char*) executableRootPath.c_str());
-            params.ssl_file_key.replace(0, executableRootPath.length(), (const char*) executableParentPathSlash.c_str());
-            printf("  - new ssl-key-file path: %s\n", (const char*) params.ssl_file_key.c_str());
-        }
-        if (starts_with(params.ssl_file_cert, executableRootPath)) {
-            printf("\n");
-            mmojo_printf("--ssl-cert-file path starts with %s.\n", (const char*) executableRootPath.c_str());
-            params.ssl_file_cert.replace(0, executableRootPath.length(), (const char*) executableParentPathSlash.c_str());
-            printf("  - new ssl-cert-file path: %s\n", (const char*) params.ssl_file_cert.c_str());
-        }
-    }
-}
 // Mmojo Server END
 
 int main(int argc, char ** argv) {
@@ -549,7 +341,7 @@ int main(int argc, char ** argv) {
     // Mmojo Server START
     main_mmojo_server_2(params);
     // Mmojo Server END
-
+  
     // validate batch size for embeddings
     // embeddings require all tokens to be processed in a single ubatch
     // see https://github.com/ggml-org/llama.cpp/issues/12836
@@ -562,7 +354,8 @@ int main(int argc, char ** argv) {
     if (params.n_parallel < 0) {
         // Mmojo Server START
         // Does this LOG_INF cause memory corruption with MinGW compile? YES.
-        // LOG_INF("%s: n_parallel is set to auto, using n_parallel = 4 and kv_unified = true\n", __func__);
+        // We're not going to do MinGW, so can remove this chnage next time. -Brad 2026-02-28
+        LOG_INF("%s: n_parallel is set to auto, using n_parallel = 4 and kv_unified = true\n", __func__);
         // Mmojo Server END
 
         params.n_parallel = 4;
@@ -571,7 +364,7 @@ int main(int argc, char ** argv) {
 
     // for consistency between server router mode and single-model mode, we set the same model name as alias
     if (params.model_alias.empty() && !params.model.name.empty()) {
-        params.model_alias = params.model.name;
+        params.model_alias.insert(params.model.name);
     }
 
     // Mmojo Server START
@@ -663,6 +456,7 @@ int main(int argc, char ** argv) {
     ctx_http.post("/v1/chat/completions", ex_wrapper(routes.post_chat_completions));
     ctx_http.post("/api/chat",            ex_wrapper(routes.post_chat_completions)); // ollama specific endpoint
     ctx_http.post("/v1/responses",        ex_wrapper(routes.post_responses_oai));
+    ctx_http.post("/responses",           ex_wrapper(routes.post_responses_oai));
     ctx_http.post("/v1/messages",         ex_wrapper(routes.post_anthropic_messages)); // anthropic messages API
     ctx_http.post("/v1/messages/count_tokens", ex_wrapper(routes.post_anthropic_count_tokens)); // anthropic token counting
     ctx_http.post("/infill",              ex_wrapper(routes.post_infill));

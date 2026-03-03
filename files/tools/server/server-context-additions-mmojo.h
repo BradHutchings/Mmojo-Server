03b05e1de4bc3afbfd7286050cc14941a584847b
Brad Hutchings <brad@componentx.com>
2026-03-03 10:38:08 -0800

    Create server-context-additions-mmojo.h

---
+++ files/tools/server/server-context-additions-mmojo.h
@@ -0,0 +1,13 @@
+/*
+  server-additions-mmojo.cpp as an adjunct file for tools/server/server-context-mmojo.h.
+
+  This file has been added to implement Mmojo Server specific fixes and features.
+
+  -Brad 2026-03-03
+  --
+  Brad Hutchings
+  brad@bradhutchings.com
+*/
+
+#include "server-http.h"
+#include "server-models.h"

c4f091cfd4e8b3edafb3edeb297931c8845aa156
Brad Hutchings <brad@componentx.com>
2026-03-04 21:56:54 -0800

    Update 410-Convert-HF-to-gguf-Model.sh

--- scripts/410-Convert-HF-to-gguf-Model.sh
+++ scripts/410-Convert-HF-to-gguf-Model.sh
@@ -8,26 +8,24 @@
 ################################################################################
 
 SCRIPT_NAME=$(basename -- "$0")
-printf "\n$STARS\n*\n* STARTED: $SCRIPT_NAME $1 $2 $3 $4.\n*\n$STARS\n\n"
+printf "\n$STARS\n*\n* STARTED: $SCRIPT_NAME $1 $2 $3.\n*\n$STARS\n\n"
 
 model_name=$1
 model_type=$2
-model_mnemonic=$3
-model_repo=$4
+model_repo=$3
 
 # $model_type doesn't go in the $MODEL_SOURCE_DIR so we can reuse the download to create different versions.
-MODEL_SOURCE_DIR="$LOCAL_MODELS_DIR/$model_name"
+MODEL_SOURCE_DIR="$MODELS_DIR/$model_name"
 MODEL_FILE="$model_name-$model_type.gguf"
 
 echo "      model_name: $model_name"
 echo "      model_type: $model_type"
-echo "  model_mnemonic: $model_mnemonic"
 echo "      model_repo: $model_repo"
 echo "MODEL_SOURCE_DIR: $MODEL_SOURCE_DIR"
 echo "      MODEL_FILE: $MODEL_FILE"
 
-if [ "$model_name" != "" ] && [ "$model_type" != "" ] && [ "$model_mnemonic" != "" ] && [ "$model_repo" != "" ]; then
-    mkdir -p "$LOCAL_MODELS_DIR"
+if [ "$model_name" != "" ] && [ "$model_type" != "" ] && [ "$model_repo" != "" ]; then
+    mkdir -p "$MODELS_DIR"
 
     if [ "$MODEL_SOURCE_DIR" != "" ] &&  [ -d "$MODEL_SOURCE_DIR" ] && [ ! -f "$MODEL_SOURCE_DIR/cloned" ]; then
         echo ""
@@ -45,24 +43,15 @@ if [ "$model_name" != "" ] && [ "$model_type" != "" ] && [ "$model_mnemonic" != 
 
     echo ""
     echo "Converting to $model_name-$model_type.gguf."
-    python3 "$LOCAL_MODELS_DIR/llama.cpp/convert_hf_to_gguf.py" "$MODEL_SOURCE_DIR" \
-        --outfile "$LOCAL_MODELS_DIR/$MODEL_FILE" \
+    python3 "$MODELS_DIR/llama.cpp/convert_hf_to_gguf.py" "$MODEL_SOURCE_DIR" \
+        --outfile "$MODELS_DIR/$MODEL_FILE" \
         --outtype "$model_type"
-
-    if [ -f "$LOCAL_MODELS_DIR/$MODEL_FILE" ]; then
-        echo ""
-        echo "Updating $LOCAL_MODEL_MAP."
-        sed -i -e "/$MODEL_FILE/d" $LOCAL_MODEL_MAP
-cat << EOF >> $LOCAL_MODEL_MAP
-$MODEL_FILE $model_mnemonic
-EOF
-    fi
 
 fi
 
 cd $HOME
 
-printf "\n$STARS\n*\n* FINISHED: $SCRIPT_NAME $1 $2 $3 $4.\n*\n$STARS\n\n"
+printf "\n$STARS\n*\n* FINISHED: $SCRIPT_NAME $1 $2 $3.\n*\n$STARS\n\n"
 
 ################################################################################
 #  This is an original script for the Mmojo Server repo. It is covered by

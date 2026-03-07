f70ead9b6df5d42a6e5c7f4649eeb29a9b323868
Brad Hutchings <brad@componentx.com>
2026-03-07 10:51:27 -0800

    Update mm-repo-clone.sh

--- scripts/mm-repo-clone.sh
+++ scripts/mm-repo-clone.sh
@@ -10,20 +10,20 @@
 SCRIPT_NAME=$(basename -- "$0")
 printf "\n$STARS\n*\n* STARTED: $SCRIPT_NAME.\n*\n$STARS\n\n"
 
-# export MMOJO_SERVER_REPO_DIR="$HOME/mm-mmojo-server"
-# export MMOJO_SERVER_REPO_FILES="$MMOJO_SERVER_REPO_DIR/scripts"
+# export REPO_DIR="$HOME/mm-repo"
+# export REPO_FILES="$REPO_DIR/scripts"
 cd $HOME
-if [ "$MMOJO_SERVER_REPO_DIR" != "" ] && [ -d "$MMOJO_SERVER_REPO_DIR" ] ; then
-  rm -r -f $MMOJO_SERVER_REPO_DIR
+if [ "$REPO_DIR" != "" ] && [ -d "$REPO_DIR" ] ; then
+  rm -r -f $REPO_DIR
 fi
-mkdir -p $MMOJO_SERVER_REPO_DIR
-git clone https://github.com/BradHutchings/mmojo-server.git $MMOJO_SERVER_REPO_DIR
+mkdir -p $REPO_DIR
+git clone https://github.com/BradHutchings/mmojo-server.git $REPO_DIR
 
 # mm-repo-update-local.sh uses these variables, so set them first.
-. $MMOJO_SERVER_REPO_SCRIPTS/mm-environment-variables.sh
+. $REPO_SCRIPTS/mm-environment-variables.sh
 
 # mm-repo-update-local.sh copies mm- scripts to $HOME/mm-scripts
-. $MMOJO_SERVER_REPO_SCRIPTS/mm-repo-update-local.sh
+. $REPO_SCRIPTS/mm-repo-update-local.sh
 
 if ! grep -q "mm-env=" "$HOME/.bashrc"; then
 cat << EOF1 >> $HOME/.bashrc

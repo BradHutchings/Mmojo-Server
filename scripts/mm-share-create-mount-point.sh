24f572f1ec2e2e026300d0d1ed37f2a8fa01f3fe
Brad Hutchings <brad@componentx.com>
2026-03-07 17:39:56 -0800

    Update mm-share-create-mount-point.sh

--- scripts/mm-share-create-mount-point.sh
+++ scripts/mm-share-create-mount-point.sh
@@ -29,9 +29,9 @@ fi
 #----------------------------------------
 # Create a `mm-share-mount.sh` script.
 #----------------------------------------
-echo "Copying $SHARE_MOUNT_SCRIPT script."
-cp "$REPO_DIR_SCRIPTS/-$SHARE_MOUNT_SCRIPT" "$HOME_SCRIPTS/$SHARE_MOUNT_SCRIPT"
-chmod a+x "$HOME_SCRIPTS/$SHARE_MOUNT_SCRIPT"
+echo "Copying $SHARE_DIR_MOUNT_SCRIPT script."
+cp "$REPO_DIR_SCRIPTS/-$SHARE_DIR_MOUNT_SCRIPT" "$HOME_SCRIPTS/$SHARE_DIR_MOUNT_SCRIPT"
+chmod a+x "$HOME_SCRIPTS/$SHARE_DIR_MOUNT_SCRIPT"
 
 printf "\n$STARS\n*\n* FINISHED: $SCRIPT_NAME.\n*\n$STARS\n\n"
 

bbb1124f8414d93f54eac3ffed78dd990e6cd4a3
Brad Hutchings <brad@componentx.com>
2026-03-08 14:47:32 -0700

    Update mm-package-choose.sh

--- scripts/mm-package-choose.sh
+++ scripts/mm-package-choose.sh
@@ -33,13 +33,7 @@ if [ -d "$PACKAGES_DIR" ]; then
             echo "Unzipping into $DEPLOY_DIR."
             if [ "$DEPLOY_DIR" != "" ]; then
                 mkdir -p "$DEPLOY_DIR"
-                wd2=$(pwd)
-                cd "$DEPLOY_DIR"
-                shopt -s extglob
-                sleep 5s
-                bash -c "rm -r -f -- !(*.gguf)"
-                shopt -u extglob
-                cd $wd2
+                find . ! -name "*.gguf" -delete
             fi
             unzip "$PACKAGES_DIR/$package" -d "$DEPLOY_DIR"
         else

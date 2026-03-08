e8f5ff20aa14e574457c805fa94a7b3a4992c680
Brad Hutchings <brad@componentx.com>
2026-03-07 17:36:28 -0800

    Update -mm-share-mount.sh

--- scripts/-mm-share-mount.sh
+++ scripts/-mm-share-mount.sh
@@ -19,7 +19,7 @@ SHARE="mmojo"
 SCRIPT_NAME=$(basename -- "$0")
 printf "\n$STARS\n*\n* STARTED: $SCRIPT_NAME.\n*\n$STARS\n\n"
 
-if [ ! -d $SHARE_MOUNT_POINT ]; then
+if [ ! -d $SHARE_DIR_MOUNT_POINT ]; then
     echo "You have not created your Mmojo Share mount point."
     exit 1
 fi
@@ -34,14 +34,14 @@ if [[ $(uname -r) =~ Microsoft|WSL ]]; then
     RUNNING_IN_WSL=1
 fi
 
-if [[ ! $(findmnt $SHARE_MOUNT_POINT) ]]; then
+if [[ ! $(findmnt $SHARE_DIR_MOUNT_POINT) ]]; then
    echo "Attempting to mount Mmojo Share as cifs. You may be prompted for your share password."
-   sudo mount -t cifs -o user=$USER //$HOST/$SHARE $SHARE_MOUNT_POINT
+   sudo mount -t cifs -o user=$USER //$HOST/$SHARE $SHARE_DIR_MOUNT_POINT
 fi
-if [[ ! $(findmnt $SHARE_MOUNT_POINT) ]]; then
+if [[ ! $(findmnt $SHARE_DIR_MOUNT_POINT) ]]; then
    if [ $RUNNING_IN_WSL == 1 ]; then
         echo "Attemoting to mount Mmojo Share as drvfs. You may be prompted for your share password."
-        sudo mount -t drvfs -o user=$USER "\\\\$HOST\\$SHARE" $SHARE_MOUNT_POINT
+        sudo mount -t drvfs -o user=$USER "\\\\$HOST\\$SHARE" $SHARE_DIR_MOUNT_POINT
    fi
 fi
 

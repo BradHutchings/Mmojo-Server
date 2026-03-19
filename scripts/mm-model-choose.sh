c478168bd255fa34eaafb343453c3c6a0d224282
Brad Hutchings <brad@componentx.com>
2026-03-18 21:33:45 -0700

    Update mm-model-choose.sh

--- scripts/mm-model-choose.sh
+++ scripts/mm-model-choose.sh
@@ -10,6 +10,10 @@
 SCRIPT_NAME=$(basename -- "$0")
 # printf "\n**********\n*\n* STARTED: $SCRIPT_NAME.\n*\n**********\n\n"
 
+#-------------------------------------------------------------------------------
+# Get list of available models in the $MODELS_DIR directory.
+#-------------------------------------------------------------------------------
+
 wd=$(pwd)
 cd $MODELS_DIR
 
@@ -22,9 +26,10 @@ done
 
 cd $wd
 
+#-------------------------------------------------------------------------------
+# User picks one of the models.
+#-------------------------------------------------------------------------------
 
-echo
-echo
 echo
 echo "Please pick a model to use:"
 for ((i=0;i<${#models[@]};i++)); do
@@ -42,6 +47,23 @@ choice=""
 if [ "$opt" -gt "0" ] && [ "$opt" -le ${#models[@]} ]; then
     choice=${models[$opt-1]}
 fi
+
+#-------------------------------------------------------------------------------
+# Copy the choice.
+#-------------------------------------------------------------------------------
+
+if [ "$choice" != "" ] && [ "$DEPLOY_DIR" != "" ]; then
+    mkdir -p "$DEPLOY_DIR"
+    if [ -d "$DEPLOY_DIR" ]; then
+        rm -f "$DEPLOY_DIR"/*.gguf
+        echo "Soft linking $choice to $DEPLOY_DIR."
+        ln -s "$MODELS_DIR/$choice" "$DEPLOY_DIR/$choice"
+    fi
+fi
+
+#-------------------------------------------------------------------------------
+# Write out answer for caller to use.
+#-------------------------------------------------------------------------------
 
 echo
 echo $choice > "/tmp/${SCRIPT_NAME%.*}.out"

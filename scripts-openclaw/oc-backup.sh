f4dd0c16d9a5f05bdaffb13d5e550dc4145826cc
Brad Hutchings <brad@componentx.com>
2026-03-22 15:07:04 -0700

    Create oc-backup.sh

---
+++ scripts-openclaw/oc-backup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+################################################################################
+# This script backs up the OpenClaw installation with a sequential number prefix.
+#
+# See licensing note at end.
+################################################################################
+
+SCRIPT_NAME=$(basename -- "$0")
+printf "\n$STARS\n*\n* STARTED: $SCRIPT_NAME.\n*\n$STARS\n\n"
+
+
+printf "\n$STARS\n*\n* FINISHED: $SCRIPT_NAME.\n*\n$STARS\n\n"
+
+################################################################################
+#  This is an original script for the Mmojo Server repo. It is covered by
+#  the repo's MIT-style LICENSE:
+#
+#  https://github.com/BradHutchings/Mmojo-Server/blob/main/LICENSE
+#
+#  Copyright (c) 2025-26 Brad Hutchings.
+#  --
+#  Brad Hutchings
+#  brad@bradhutchings.com
+################################################################################

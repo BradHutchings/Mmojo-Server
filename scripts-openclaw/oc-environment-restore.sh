51cfa533e65b1e4b528a0931d0ff8daac007df02
Brad Hutchings <brad@componentx.com>
2026-03-22 19:52:16 -0700

    Update oc-environment-restore.sh

--- scripts-openclaw/oc-environment-restore.sh
+++ scripts-openclaw/oc-environment-restore.sh
@@ -9,6 +9,57 @@
 SCRIPT_NAME=$(basename -- "$0")
 printf "\n$STARS\n*\n* STARTED: $SCRIPT_NAME.\n*\n$STARS\n\n"
 
+wd=$(pwd)
+cd $HOME
+
+########################################
+# 
+########################################
+
+OPENCLAW_BACKUPS="$HOME/openclaw-backups"
+mkdir -p $OPENCLAW_BACKUPS
+
+echo
+read -p 'Please enter a name for this backup: ' backupName
+echo
+
+backupName="${backupName// /-}"
+prefix=""
+for i in $(seq -f "%02g" 1 99); do
+    if [ ! -f "$OPENCLAW_BACKUPS/$i"* ]; then
+        prefix=$i
+        break;
+    fi
+done
+
+if [ "$prefix" != "" ]; then
+    backupFile="$i-$backupName.zip"
+    echo "Backing up to: $backupFile"
+    touch "$OPENCLAW_BACKUPS/$backupFile"
+else
+    echo "Backups are full."
+    backupName=""
+fi
+
+if ["$backupName" != "" ]; then
+    if [ ! -f "$OPENCLAW_BACKUPS/$backupName" ]; then
+        isRunning=($(oc-gateway-status.sh) = "Running")
+        if ($isRunning); then
+            oc-gateway-stop.sh
+        fi
+    
+        zip -r "$backupName" .openclaw
+        mv "$backupName" $OPENCLAW_BACKUPS
+    
+        if ($isRunning); then
+            oc-gateway-start.sh
+        fi
+    else
+        echo "The file $OPENCLAW_BACKUPS/$backupName already exists. It was not overwritten."
+    fi
+fi
+
+cd $pwd
 
 printf "\n$STARS\n*\n* FINISHED: $SCRIPT_NAME.\n*\n$STARS\n\n"
 

e1272e914b55e4b7bd357b0e52401636b85546ff
Brad Hutchings <brad@componentx.com>
2026-04-02 19:19:49 -0700

    Update arg-mmojo.cpp

--- files/common/arg-mmojo.cpp
+++ files/common/arg-mmojo.cpp
@@ -4018,7 +4018,7 @@ common_params_context common_params_parser_init(common_params & params, llama_ex
         }
     ).set_examples({LLAMA_EXAMPLE_SERVER}));
     // Mmojo Server END  
-
+    
     return ctx_arg;
 }
 

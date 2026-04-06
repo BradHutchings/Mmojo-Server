5be06051452430fe91b83fa4ad405e176c5c26be
Brad Hutchings <brad@componentx.com>
2026-04-05 22:07:53 -0700

    Update oc-patch-system-prompt-remove.sh

--- scripts-openclaw/oc-patch-system-prompt-remove.sh
+++ scripts-openclaw/oc-patch-system-prompt-remove.sh
@@ -14,46 +14,30 @@ SCRIPT_NAME=$(basename -- "$0")
 # patch does not exist.
 ################################################################################
 
-PATCH_FILE="$REPO_DIR/files-openclaw/patches/buildAgentSystemPrompt.js"
-FUNCTION_DEF=""
-START_SEARCH_1="function buildAgentSystemPrompt(params)"
-START_SEARCH_2="function buildAgentSystemPrompt_inactive(params)"
-END_SEARCH="function buildRuntimeLine"
 DIST_DIR="$HOME/.npm-global/lib/node_modules/openclaw/dist"
-PATCH_SEARCH="Mmojo Patch START"
-SOURCE_FILE=$(grep -lr --include="*.js" "$START_SEARCH" "$DIST_DIR")
-if [ -f "$SOURCE_FILE" ]; then
-    echo "SOURCE_FILE: $SOURCE_FILE."
-    START_LINE_1_GREP=$(grep -n "$START_SEARCH_1" "$SOURCE_FILE")
-    START_LINE_1=${START_LINE_1_GREP%%:*}
-    START_LINE_2_GREP=$(grep -n "$START_SEARCH_2" "$SOURCE_FILE")
-    START_LINE_2=${START_LINE_2_GREP%%:*}
-    END_LINE_GREP=$(grep -n "$END_SEARCH" "$SOURCE_FILE")
-    END_LINE=$(( ${END_LINE_GREP%%:*}-1 ))
-    PATCH_LINE_GREP=$(grep -n "$PATCH_SEARCH" "$SOURCE_FILE")
+PATCHED_SEARCH="Mmojo Patch START - buildAgentSystemPrompt"
+PATCHED_FILE=$(grep -lr --include="*.js" "$PATCHED_FILE" "$DIST_DIR")
+INACTIVE_FUNCTION="function buildAgentSystemPrompt_inactive(params)"
+ACTIVE_FUNCTION="function buildAgentSystemPrompt(params)"
+if [ -f "$PATCHED_FILE" ]; then
+    echo "PATCHED_FILE: $PATCHED_FILE."
+    PATCH_LINE_GREP=$(grep -n "$PATCHED_SEARCH" "$PATCHED_FILE")
     PATCH_LINE=${PATCH_LINE_GREP%%:*}
-    echo "START_LINE_1_GREP: $START_LINE_1_GREP"
-    echo "START_LINE_1: $START_LINE_1"
-    echo "START_LINE_2_GREP: $START_LINE_2_GREP"
-    echo "START_LINE_2: $START_LINE_2"
-    echo "END_LINE_GREP: $END_LINE_GREP"
-    echo "END_LINE: $END_LINE"
     echo "PATCH_LINE_GREP: $PATCH_LINE_GREP"
     echo "PATCH_LINE: $PATCH_LINE"
     echo
-    if ( [ "$START_LINE_1" -gt "0" ] || [ "$START_LINE_2" -gt "0" ] ) && [ "$END_LINE" -gt "0" ]; then
-        if [ "$PATCH_LINE" = "" ]; then
-            echo "Not patched: $FILE."
-        else
-            # Remove the patch at the bottom
-            # Restore the function name.
-            echo "Removing patch: $SOURCE_FILE."
-            $MMOJO_SED -i -e "/$PATCH_SEARCH/,\$d" "$SOURCE_FILE"
-            $MMOJO_SED -i -e "s/$START_LINE_2/$START_LINE_1/g" "$SOURCE_FILE"
-        fi
+    if [ "$PATCH_LINE" -gt "0" ]; then
+        # Remove the patch at the bottom
+        # Restore the function name.
+        echo "Removing patch: $PATCHED_FILE."
+        $MMOJO_SED -i -e "/$PATCH_SEARCH/,\$d" "$PATCHED_FILE"
+        $MMOJO_SED -i -e "s/$INACTIVE_FUNCTION/$ACTIVE_FUNCTION/g" "$PATCHED_FILE"
     else
-        echo "No source file was found."
+        # Should never happen.
+        echo "Not patched: $PATCHED_FILE."
     fi
+else
+    echo "No patched file was found."
 fi
 
 # printf "\n$STARS\n*\n* FINISHED: $SCRIPT_NAME.\n*\n$STARS\n\n"

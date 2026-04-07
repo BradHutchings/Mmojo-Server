e2cb71af811291d7f4b2acd070e7c5683f872f31
Brad Hutchings <brad@componentx.com>
2026-04-07 14:06:50 -0700

    Create mm-repo-copy-scripts.sh

---
+++ scripts/mm-repo-copy-scripts.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+################################################################################
+# This script switches your local clone of the Mmojo Server repo to the main
+# branch, then copies all of the mm- scripts to $HOME/scripts.
+#
+# See licensing note at end.
+################################################################################
+
+SCRIPT_NAME=$(basename -- "$0")
+# printf "\n$STARS\n*\n* STARTED: $SCRIPT_NAME.\n*\n$STARS\n\n"
+
+### Links don't work - end up modifying repo files on chmod.
+if [ -d "$HOME_SCRIPTS" ]; then
+    cp $REPO_DIR_SCRIPTS/mm-*.sh $HOME_SCRIPTS
+    chmod a+x $HOME_SCRIPTS/mm-*.sh
+
+    # on darwin, change #!/bin/bash to "#!$HOME/homebrew/bin/bash"
+    # $MMOJO_SED -i -e "1c\#!/usr/local/bash" $HOME_SCRIPTS/mm-*.sh
+    $MMOJO_SED -i -e "1c\#!$HOME/homebrew/bin/bash" $HOME_SCRIPTS/mm-*.sh
+fi
+
+if [ -d "$HOME_OC_SCRIPTS" ]; then
+    cp $REPO_DIR_SCRIPTS_OPENCLAW/oc-*.sh $HOME_OC_SCRIPTS
+    cp $REPO_DIR_SCRIPTS/mm-environment-variables.sh $HOME_OC_SCRIPTS
+    cp $REPO_DIR_SCRIPTS/mm-repo-*.sh $HOME_OC_SCRIPTS
+    chmod a+x $HOME_OC_SCRIPTS/mm-*.sh
+    chmod a+x $HOME_OC_SCRIPTS/oc-*.sh
+    $MMOJO_SED -i -e "1c\#!$HOME/homebrew/bin/bash" $HOME_OC_SCRIPTS/mm-*.sh
+    $MMOJO_SED -i -e "1c\#!$HOME/homebrew/bin/bash" $HOME_OC_SCRIPTS/oc-*.sh
+fi
+
+# printf "\n$STARS\n*\n* FINISHED: $SCRIPT_NAME.\n*\n$STARS\n\n"
+
+################################################################################
+#  This is an original script for the Mmojo Server repo. It is covered by
+#  the repo's MIT-style LICENSE:
+#
+#  https://github.com/BradHutchings/Mmojo-Server/blob/main/LICENSE
+#
+#  Copyright (c) 2025-26 Brad Hutchings.
+#  --
+#  Brad Hutchings
+#  brad@bradhutchings.com
+################################################################################

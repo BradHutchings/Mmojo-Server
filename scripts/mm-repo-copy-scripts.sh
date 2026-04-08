5f79e98eca1d85bdbbb9e17720f616e0ec44e7ab
Brad Hutchings <brad@componentx.com>
2026-04-08 08:39:58 -0700

    Update mm-repo-copy-scripts.sh

--- scripts/mm-repo-copy-scripts.sh
+++ scripts/mm-repo-copy-scripts.sh
@@ -24,7 +24,7 @@ if [ -d "$HOME_SCRIPTS" ]; then
 
     # on darwin, change "/bin/bash" to "/usr/local/bash"
     if [ "$(uname -s)" = "Darwin" ]; then
-        $MMOJO_SED -i -e '1c#!/usr/local/bash' $HOME_SCRIPTS/mm-*.sh
+        $MMOJO_SED -i -e '1s|/bin/bash|/usr/local/bin|' $HOME_SCRIPTS/mm-*.sh
     fi
 fi
 

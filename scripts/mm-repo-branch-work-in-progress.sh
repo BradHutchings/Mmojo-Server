2074bdc5493ccc3225dfd6c09a9fa6990940350e
Brad Hutchings <brad@componentx.com>
2026-04-08 07:46:16 -0700

    Update mm-repo-branch-work-in-progress.sh

--- scripts/mm-repo-branch-work-in-progress.sh
+++ scripts/mm-repo-branch-work-in-progress.sh
@@ -16,17 +16,6 @@ if [ -d "$REPO_DIR" ]; then
   git reset --hard
   git checkout work-in-progress
   cd $WD
-
-  # DELETE THIS BLOCK WHEN THE SCRIPTS ARE GONE. -Brad 2026-03-16
-  # These are the scripts. They need to be executable.
-  chmod -f a+x $REPO_DIR_SCRIPTS/3*.sh
-  chmod -f a+x $REPO_DIR_SCRIPTS/4*.sh
-
-  ### Links don't work - end up modifying repo files on chmod.
-  if [ -d "$HOME_SCRIPTS" ]; then
-      cp $REPO_DIR_SCRIPTS/mm-*.sh $HOME_SCRIPTS
-      chmod a+x $HOME_SCRIPTS/mm-*.sh
-  fi
   
   mm-repo-copy-scripts.sh
 else

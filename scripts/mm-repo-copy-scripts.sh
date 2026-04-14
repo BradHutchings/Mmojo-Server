164cba25a9f8966125884014d9bc345bdca82591
Brad Hutchings <brad@componentx.com>
2026-04-14 11:47:00 -0700

    Update mm-repo-copy-scripts.sh

--- scripts/mm-repo-copy-scripts.sh
+++ scripts/mm-repo-copy-scripts.sh
@@ -19,12 +19,12 @@ if [ -d "$HOME_SCRIPTS" ]; then
 fi
 
 if [ -d "$HOME_OC_SCRIPTS" ]; then
-    cp "$REPO_DIR_SCRIPTS_OPENCLAW/"oc-*.sh "$HOME_OC_SCRIPTS"
+    cp "$REPO_DIR_SCRIPTS_OPENCLAW/oc-"*.sh "$HOME_OC_SCRIPTS"
     cp "$REPO_DIR_SCRIPTS/mm-environment-variables.sh" "$HOME_OC_SCRIPTS"
-    cp "$REPO_DIR_SCRIPTS/"mm-repo-*.sh "$HOME_OC_SCRIPTS"
-    cp "$REPO_DIR_SCRIPTS/"mm-share-*.sh "$HOME_OC_SCRIPTS"
-    chmod a+x "$HOME_OC_SCRIPTS/"mm-*.sh
-    chmod a+x "$HOME_OC_SCRIPTS/"oc-*.sh
+    cp "$REPO_DIR_SCRIPTS/mm-repo-"*.sh "$HOME_OC_SCRIPTS"
+    cp "$REPO_DIR_SCRIPTS/mm-share-"*.sh "$HOME_OC_SCRIPTS"
+    chmod a+x "$HOME_OC_SCRIPTS/mm-"*.sh
+    chmod a+x "$HOME_OC_SCRIPTS/oc-"*.sh
 fi
 
 cd "$wd"

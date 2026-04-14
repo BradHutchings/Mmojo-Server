ed8b0eb610e9b8e1731a96c7368fc6fd13cc07d0
Brad Hutchings <brad@componentx.com>
2026-04-14 16:38:28 -0700

    Update mm-mmojo-server-start.sh

--- scripts/mm-mmojo-server-start.sh
+++ scripts/mm-mmojo-server-start.sh
@@ -19,7 +19,7 @@ if [ "$background" != "" ]; then
     runInBackground=true;
 fi
 
-echo "- \$background: $background"
+echo "- \$background: \"$background\""
 echo "- \$runInBackground: $runInBackground"
 echo "- \$DEPLOY_DIR: $DEPLOY_DIR"
 echo "- \$_PACKAGE_MMOJO_SERVER_FILE: $_PACKAGE_MMOJO_SERVER_FILE"

a4aab7d612c92cf260d4f815f10ba5abee3f5c53
Brad Hutchings <brad@componentx.com>
2026-05-04 13:11:57 -0700

    Update oc-gateway-status.sh

--- scripts-openclaw/oc-gateway-status.sh
+++ scripts-openclaw/oc-gateway-status.sh
@@ -11,6 +11,23 @@ SCRIPT_NAME=$(basename -- "$0")
 
 if [ $MMOJO_DARWIN ]; then
     running=$(launchctl print gui/$(id -u openclaw)/ai.openclaw.gateway | grep "state = running")
+
+    local_list=$(launchctl list | grep ai.openclaw.gateway)
+    local_is_running=false
+    if [ "$local_list" != "" ]; then
+        local_is_running=true
+    fi
+        
+    system_list=$(sudo launchctl list | grep ai.openclaw.gateway)
+    system_is_running=false
+    if [ "$system_list" != "" ]; then
+        system_is_running=true
+    fi
+
+    running=false
+    if [ "$local_is_running" == "true" ] || [ "$system_is_running" == "true" ]; then
+        running=true
+    fi
 else
     running=$(systemctl --user status openclaw-gateway.service | grep "running")
 fi

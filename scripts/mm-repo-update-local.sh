8aafcacb9814d52af15ae7279ef026a4f2e1fdc4
Brad Hutchings <brad@componentx.com>
2026-07-17 22:45:58 -0700

    Update mm-repo-update-local.sh

--- scripts/mm-repo-update-local.sh
+++ scripts/mm-repo-update-local.sh
@@ -23,12 +23,6 @@ if [ -d "$REPO_DIR" ]; then
             # echo "Fixing $file."
             $MMOJO_SED -i -e '1c#!/usr/local/bin/bash' "$file"
         done
-
-        # cd "$REPO_DIR_SCRIPTS_OPENCLAW"
-        # for file in oc*.sh; do
-        #     # echo "Fixing $file."
-        #     $MMOJO_SED -i -e '1c#!/usr/local/bin/bash' "$file"
-        # done
     fi
 
     parent=$(dirname -- $0)

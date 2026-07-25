4ec1ccea97e63d7b6aa5c7d0bc620f349e0128ef
Brad Hutchings <brad@componentx.com>
2026-07-25 08:19:01 -0700

    Update mm-mmojo-server-start.sh

--- scripts/mm-mmojo-server-start.sh
+++ scripts/mm-mmojo-server-start.sh
@@ -116,5 +116,5 @@ fi
 #  Copyright (c) 2025-26 Brad Hutchings.
 #  --
 #  Brad Hutchings
-#  brad@bradhutchings.com
+#  brad@Mmojo.net
 ################################################################################

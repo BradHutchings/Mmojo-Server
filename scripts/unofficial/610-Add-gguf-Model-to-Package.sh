57b1f6d173c546d87f57a58cc3224c1e7d72450a
Brad Hutchings <brad@componentx.com>
2026-07-25 07:50:34 -0700

    Update 610-Add-gguf-Model-to-Package.sh

--- scripts/unofficial/610-Add-gguf-Model-to-Package.sh
+++ scripts/unofficial/610-Add-gguf-Model-to-Package.sh
@@ -36,5 +36,5 @@ printf "\n$STARS\n*\n* FINISHED: $SCRIPT_NAME.\n*\n$STARS\n\n"
 #  Copyright (c) 2025-26 Brad Hutchings.
 #  --
 #  Brad Hutchings
-#  brad@bradhutchings.com
+#  brad@Mmojo.net
 ################################################################################

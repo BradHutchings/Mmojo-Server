f599d90f085059a656508eaf3c37970b058db79a
Brad Hutchings <brad@componentx.com>
2026-07-25 08:16:20 -0700

    Update oc-gateway-stop.sh

--- scripts-openclaw/oc-gateway-stop.sh
+++ scripts-openclaw/oc-gateway-stop.sh
@@ -60,5 +60,5 @@ fi
 #  Copyright (c) 2025-26 Brad Hutchings.
 #  --
 #  Brad Hutchings
-#  brad@bradhutchings.com
+#  brad@Mmojo.net
 ################################################################################

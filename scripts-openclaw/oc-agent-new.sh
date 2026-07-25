c9e87a8cebfee866123d253a14010b842762fc35
Brad Hutchings <brad@componentx.com>
2026-07-25 07:54:16 -0700

    Update oc-agent-new.sh

--- scripts-openclaw/oc-agent-new.sh
+++ scripts-openclaw/oc-agent-new.sh
@@ -43,5 +43,5 @@ cd $pwd
 #  Copyright (c) 2025-26 Brad Hutchings.
 #  --
 #  Brad Hutchings
-#  brad@bradhutchings.com
+#  brad@Mmojo.net
 ################################################################################

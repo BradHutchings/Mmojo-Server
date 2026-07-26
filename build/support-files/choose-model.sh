4eb5801b936e198b6f8c7a5ed8b3ffb85e0eced9
Brad Hutchings <brad@componentx.com>
2026-07-26 12:00:54 -0700

    Create choose-model.sh

---
+++ build/support-files/choose-model.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+################################################################################
+#  choose-model.sh
+#
+#  This script chooses a model in the models subdirectory and soft links it into 
+#  the parent directory of this script.
+#
+#  It safely cleans up the parent directory of this script as well.
+#
+#  As you can probably tell, there is work to be done on this script. LOL.
+################################################################################
